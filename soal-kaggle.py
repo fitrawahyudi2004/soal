e7918e2ef96bd37ad79475647b7f1e606201cd87
fitrawahyudi2004 <76257003+fitrawahyudi2004@users.noreply.github.com>
2023-11-11 15:49:32 +0700

    Create soal-kaggle.py

---
+++ soal-kaggle.py
@@ -0,0 +1,14 @@
+def operasi_aritmatika(num1, num2):
+    # Penjumlahan
+    hasil_penjumlahan = num1 + num2
+
+    # Pengurangan
+    hasil_pengurangan = num1 - num2
+
+    # Perkalian
+    hasil_perkalian = num1 * num2
+
+    # Pembagian
+    hasil_pembagian = num1 / num2
+
+    return hasil_penjumlahan, hasil_pengurangan, hasil_perkalian, hasil_pembagian
