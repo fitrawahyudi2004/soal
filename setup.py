e5fb3ef13b7f10ab9355c1e912b2dfdb2275f75f
fitrawahyudi2004 <76257003+fitrawahyudi2004@users.noreply.github.com>
2023-11-11 15:52:42 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import soalfitra
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='soalfitra',
+      version=learntools.__version__,
+      description='Utilities for Kaggle Learn exercises',
+      url='https://github.com/fitrawahyudi2004/soal',
+      author='fwd',
+      author_email='fwd@gmail.com',
+      license='Apache 2.0',
+      packages=find_packages(),
+      zip_safe=True)
